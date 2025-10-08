2b22b9159910e581a94baefd9cc926166c026d6c
ssoumenmaity3 <sm8039912@gmail.com>
2025-10-08 11:48:51 +0530

    Missing

--- src/main/java/com/soumen/upi/Service/JWTService.java
+++ src/main/java/com/soumen/upi/Service/JWTService.java
@@ -5,12 +5,15 @@ import java.util.Date;
 import java.util.function.Function;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.io.Decoders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
 
 
 @Service
@@ -25,13 +28,15 @@ public class JWTService {
         return Jwts.builder()
                 .subject(username)
                 .claim("email", username)
+                .header().empty().add("typ","JWT")
+                .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
                 .signWith(getKey())
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
